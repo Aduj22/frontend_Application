d0611cc75018d82135152b603e7fa77d8c742ae9
Adil <aduj22@bth.se>
2025-09-30 02:31:38 +0200

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/frontend_Application">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/docs" element={<Home />}></Route>
           <Route path="/docs/:id" element={<View_doc />}></Route>
-          <Route path="docs/add" element={<Add_doc />}></Route>
+          <Route path="/docs/add" element={<Add_doc />}></Route>
         </Routes>
       </BrowserRouter>
     </>
